b1d000143fcdbdf8a4dc34d0d1657e81a75da55b
Chae, KyungWan <house05240@naver.com>
2025-05-21 09:54:11 +0900

    five

--- Main.java
+++ Main.java
@@ -1,1 +1,4 @@
-public class Main { public static void main(String[] args) { System.out.println("채경완"); }
+public class Main { public static void main(String[] args) { System.out.println("채경완"); 
+World.run(); 
+World.run2();
+}
