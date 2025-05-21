9f7b0ce37c02ff8b2cf168b8407f209a518bdc9d
Chae, KyungWan <house05240@naver.com>
2025-05-21 09:59:25 +0900

    six

--- World.java
+++ World.java
@@ -1,4 +1,5 @@
 public class World {
     public static void run() { System.out.println("201514099"); } 
     public static void run2() { System.out.println("hello world"); } 
+    public static void run3() { System.out.println("안녕하세요"); }
 }
